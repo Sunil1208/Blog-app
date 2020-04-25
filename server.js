e716747385801a133a62595442f38c4136090cff
Sunil1208 <skmy03@gmail.com>
2020-04-25 20:30:04 +0530

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require('dotenv').config(); //for loading the environment
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 const mongoose = require('mongoose');
 const passport = require('passport')
 
